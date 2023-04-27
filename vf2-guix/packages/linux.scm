2e668b846ed41bcd6a19b7a6853c10685a2a7fb9
Aleksandr Vityazev <avityazev@posteo.org>
2023-04-27 09:00:44 +0300

    vf2-guix: linux: switch to upstream branch.
    * vf2-guix/packages/linux (linux-vf2-upstream): New procedure.
    vf2-kernel-upstream: New package.
    vf2-kernel: Remove.
    
    # ---
    # Files to be committed to branch `master' tracking `No Remote Found':
    #
    #   vf2-guix/packages/linux.scm
    #

--- vf2-guix/packages/linux.scm
+++ vf2-guix/packages/linux.scm
@@ -15,12 +15,18 @@
    (string-append
     "https://github.com/starfive-tech/linux/archive/refs/tags/" version ".tar.gz")))
 
+(define (linux-vf2-upstream commit)
+  (git-reference
+   (url "https://github.com/starfive-tech/linux")
+   (commit commit)))
+
 (define* (linux-vf2
-          version
+          commit
           hash
+          version
           #:key
           (name "linux-vf2")
-          (defconfig "starfive_visionfive2_defconfig")
+          (defconfig "defconfig")
           (linux linux-libre-riscv64-generic))
   (let ((linux-package
          (customize-linux
@@ -28,9 +34,15 @@
           #:linux linux
           #:defconfig defconfig
           #:extra-version "starfive-visionfive2"
-          #:source (origin (method url-fetch)
-                           (uri (linux-vf2-urls version))
-                           (sha256 (base32 hash))))))
+          #:source (origin
+                     (method git-fetch)
+                     (uri (linux-vf2-upstream commit))
+                     (file-name (git-file-name name version))
+                     (sha256 (base32 hash)))
+          ;; (origin (method url-fetch)
+          ;;         (uri (linux-vf2-urls version))
+          ;;         (sha256 (base32 hash)))
+          )))
     (package
       (inherit linux-package)
       (version version)
@@ -38,22 +50,31 @@
        (substitute-keyword-arguments (package-arguments linux-package)
          ((#:phases phases '%standard-phases)
           #~(modify-phases #$phases
-              (add-after 'unpack 'increase-memory
-                (lambda _
-                  (substitute* "arch/riscv/boot/dts/starfive/jh7110-visionfive-v2.dtsi"
-                    (("0x0 0x40000000 0x1 0x0") "0x0 0x40000000 0x2 0x0"))))
+              ;; (add-after 'unpack 'increase-memory
+              ;;   (lambda _
+              ;;     (substitute* "arch/riscv/boot/dts/starfive/jh7110-visionfive-v2.dtsi"
+              ;;       (("0x0 0x40000000 0x1 0x0") "0x0 0x40000000 0x2 0x0"))))
               (add-after 'install 'rename-dtb
                 (lambda* (#:key outputs #:allow-other-keys)
                   (let* ((out (assoc-ref outputs "out"))
                          (dtbs (string-append out "/lib/dtbs/starfive")))
                     (with-directory-excursion dtbs
-                      (rename-file "jh7110-visionfive-v2.dtb" "starfive_visionfive2.dtb")))))))))
+                      (rename-file "jh7110-starfive-visionfive-2-v1.2a.dtb"
+                                   "starfive_visionfive2.dtb")))))))))
+      (inputs (modify-inputs (package-inputs linux-package)
+                (prepend zstd)))
       (home-page "https://www.kernel.org/")
       (synopsis "Linux kernel with nonfree binary blobs included")
       (description
        "The unmodified Linux kernel, including nonfree blobs, for running Guix
 System on hardware which requires nonfree software to function."))))
 
-(define-public vf2-kernel
-  (linux-vf2 "VF2_v2.11.5"
-             "1h0zac2szbxyv8dp0sm0bfrprjn09j3icjsl7jp35bsz336g4vla"))
+
+;; (define-public vf2-kernel
+;;   (linux-vf2 "VF2_v2.11.5"
+;;              "1h0zac2szbxyv8dp0sm0bfrprjn09j3icjsl7jp35bsz336g4vla"))
+
+(define-public vf2-kernel-upstream
+  (linux-vf2 "d768c65c1daaca83ff6b8d119ca50ff9d8273b22"
+             "1fvbjr74spm6iig0b2nksmrzzas6zlsv7h94w0546jdvpjpckgw6"
+             "VF2_v2.11.5"))
